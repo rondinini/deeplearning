fc8719ebb1c5a772c683bd3fbc10c6c38ad4c885
Gianluca Rondinini <gianrond@macbookpro.home>
2022-12-11 18:21:44 +0100

    added resnet block

--- src/models/layers.py
+++ src/models/layers.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Layer, Conv2D, BatchNormalization, Dropout, Activation, MaxPool2D
+from tensorflow.keras.layers import Layer, Conv2D, BatchNormalization, Dropout, Activation, MaxPool2D, Add
 
 
 class CNNBlock(Layer):
@@ -26,3 +26,31 @@ class CNNBlock(Layer):
         if self.max_pool:
             x = self.max_pool(x)
         return x
+
+
+class ResNetBlock(Layer):
+
+    def __init__(self, **kwargs):
+        super(ResNetBlock, self).__init__()
+        # Strides need to match for first and last cnn layer
+        self.cnn_block_one = CNNBlock(**kwargs)
+        self.cnn_block_skip = CNNBlock(**kwargs)
+        # Strides can't change in between the ResNet
+        kwargs['strides'] = 1
+        self.cnn_block_two = CNNBlock(**kwargs)
+        self.act_out = Activation('relu')
+        self.bn_out = BatchNormalization()
+
+    def call(self, inputs):
+        x = self.cnn_block_one(inputs)
+        x = self.cnn_block_two(x)
+
+        skipped_x = self.cnn_block_skip(inputs)
+
+        output = Add()([x, skipped_x])
+        output = self.bn_out(output)
+        output = self.act_out(output)
+
+        return output
+
+
--- src/projects/domainnet/architecture.py
+++ src/projects/domainnet/architecture.py
@@ -4,6 +4,7 @@ from tensorflow.keras import Input
 from tensorflow.keras.optimizers import Adam
 
 from src.models.models import FunctionalModel
+from src.models.layers import ResNetBlock
 
 optimizer = Adam(learning_rate=0.001)
 
@@ -131,3 +132,52 @@ class CNNDomainNet(FunctionalModel):
         model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         
         return model
+
+class CnnResBlock(FunctionalModel):
+
+    def __init__(self, **args):
+        super(CnnResBlock, self).__init__(**args)
+        self.resnet_one_params = {
+            'filters': 64,
+            'kernel_size': 5,
+            'padding': 'same',
+            'activation': 'relu',
+            'dropout': 0.3,
+            'use_max_pool': False,
+        }
+        self.resnet_two_params = {
+            'filters': 128,
+            'kernel_size': 3,
+            'padding': 'same',
+            'activation': 'relu',
+            'dropout': 0.3,
+            'use_max_pool': False,
+        }
+        self.resnet_three_params = {
+            'filters': 256,
+            'kernel_size': 3,
+            'padding': 'same',
+            'activation': 'relu',
+            'dropout': 0.3,
+            'use_max_pool': False,
+        }
+
+    def connect_layers(self):
+        input_layer = Input(shape=(128, 128, 3))
+        resnet = ResNetBlock(**self.resnet_one_params)(input_layer)
+        resnet = MaxPool2D(pool_size=2)(resnet)
+        resnet = ResNetBlock(**self.resnet_two_params)(resnet)
+        resnet = MaxPool2D(pool_size=2)(resnet)
+        resnet = ResNetBlock(**self.resnet_three_params)(resnet)
+        resnet = MaxPool2D(pool_size=2)(resnet)
+
+        resnet = Flatten()(resnet)
+
+        dense_output = Dense(345)(resnet)
+        output = Activation('softmax')(dense_output)
+
+        model = self._build_model(inputs=input_layer, outputs=output)
+        model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        
+        return model
+
