c7b8e39fdf777c84bd03a28842f14c27f86f92ae
Gianluca Rondinini <gianrond@macbookpro-1.home>
2023-01-06 18:00:16 +0100

    working on a GAN for eurosat

--- src/projects/eurosat/architecture.py
+++ src/projects/eurosat/architecture.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, BatchNormalization, Dropout, Activation, GlobalMaxPooling2D
+from tensorflow.keras.layers import (Dense, Conv2D, MaxPool2D, Flatten, BatchNormalization, 
+Dropout, Activation, GlobalMaxPooling2D, Reshape, Conv2DTranspose, LeakyReLU)
 from tensorflow.keras import Input
 from tensorflow.keras.optimizers.legacy import Adam
 
@@ -38,7 +39,7 @@ class FCNNetwork(FunctionalModel):
         conv = MaxPool2D(pool_size=2)(conv)
 
         conv = Conv2D(
-            filters=256, 
+            filters=128, 
             kernel_size=3, 
             padding='same',
         )(conv)
@@ -160,4 +161,86 @@ class CnnResBlock(FunctionalModel):
         model = self._build_model(inputs=input_layer, outputs=output)
         model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         
+        return model
+
+
+class GANGenerator(FunctionalModel):
+
+    def __init__(self, **args):
+        super(GANGenerator, self).__init__(**args)
+
+    def connect_layers(self, codings_size):
+        input_layer = Input(shape=(codings_size, ))
+        x = Dense(64 * 64 * 128)(input_layer)
+        x = Reshape((64, 64, 128))(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2DTranspose(
+            filters=64, 
+            kernel_size=5, 
+            strides=2, 
+            padding='same'
+        )(x)
+        x = Activation('relu')(x)
+        x = BatchNormalization()(x)
+        
+        x = Conv2DTranspose(
+            filters=3,
+            kernel_size=5,
+            strides=2,
+            padding='same',
+        )(x)
+        output = Activation('tanh')(x)
+        model = self._build_model(inputs=input_layer, outputs=output)
+        return model
+
+
+class GANDiscriminator(FunctionalModel):
+
+    def __init__(self, **args):
+        super(GANDiscriminator, self).__init__(**args)
+
+    def connect_layers(self, batch_input_shape: tuple):
+        input_layer = Input(batch_input_shape=batch_input_shape)
+        x = Conv2D(
+            filters=64,
+            kernel_size=5,
+            strides=2,
+            padding='same',
+        )(input_layer)
+        x = LeakyReLU(0.2)(x)
+        x = Dropout(0.4)(x)
+        
+        x = Conv2D(
+            filters=128,
+            kernel_size=5,
+            strides=2,
+            padding='same',
+        )(x)
+        x = LeakyReLU(0.2)(x)
+        x = Dropout(0.4)(x)
+        x = Flatten()(x)
+
+        output = Dense(1, activation='sigmoid')(x)
+
+        model = self._build_model(inputs=input_layer, outputs=output)
+        model.compile(optimizer='rmsprop', loss='binary_crossentropy')
+        return model
+
+
+class GAN(FunctionalModel):
+
+    def __init__(self, generator, discriminator, **args):
+        super(GAN, self).__init__(**args)
+        self.generator = generator
+        self.discriminator = discriminator
+
+    def connect_layers(self, codings_size):
+        input_layer = Input(shape=(codings_size, ))
+    
+        generator_output = self.generator(input_layer)
+        discriminator_output = self.discriminator(generator_output)
+
+        model = self._build_model(inputs=input_layer, outputs=discriminator_output)
+        model.compile(loss='binary_crossentropy', optimizer='rmsprop')
         return model
